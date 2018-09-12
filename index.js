6886bc2ac1f577b93de77e4ede3e1eb674e3c2f0
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-12 16:32:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 
-let mostProfitableNeighborhood = 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
